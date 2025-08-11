d05f20ae079ccadb210b4884df9f9f67ffb5e664
namgigun <namgigun@gmail.com>
2025-08-11 17:23:26 +0900

    Test: 삭제 예외처리 테스트 코드 추가
    - 1번 명언을 삭제한다고 가정하였음.
    - 존재하지 않는 명언을 삭제하는 경우, "N번 명언은 존재하지 않는다." 메시지 출력

--- p-13976-1/src/test/kotlin/AppTest.kt
+++ p-13976-1/src/test/kotlin/AppTest.kt
@@ -96,4 +96,24 @@ class AppTest {
 
         assertThat(result).contains("1번 명언이 삭제되었습니다.")
     }
+
+    @Test
+    @DisplayName("존재하지 않는 명언삭제에 대한 예외처리")
+    fun t7() {
+        val result = TestRunner.run("""
+            등록
+            현재를 사랑하라.
+            작자미상
+            등록
+            과거에 집착하지 마라.
+            작자미상
+            목록
+            삭제?id=1
+            삭제?id=1
+            종료
+        """)
+
+        assertThat(result).contains("1번 명언이 삭제되었습니다.")
+        assertThat(result).contains("1번 명언은 존재하지 않습니다.")
+    }
 }
